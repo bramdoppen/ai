24d6c326e6289c372a1e765582c5142eb7265b24
bramdoppen <bramdoppen@gmail.com>
2020-11-18 06:29:21 +0100

    Some funky styling and animations

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Ai" src="./assets/ai-1.gif">
+    <transition name="fade">
+      <form
+      id="formOne"
+      @submit="save"
+      action="https://vuejs.org/"
+      method="post"
+      v-if="!nameSaved">
+        <input v-model="myName" placeholder="Your name" required>
+        <button>Feed it</button>
+      </form>
+    </transition>
+    
+    <Message :userName="myName" v-if="nameSaved" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Message from "./components/Message.vue";
 
 export default {
   name: 'App',
+  data: () => {
+    return {
+      myName: null,
+      nameSaved: false
+    }
+  },
   components: {
-    HelloWorld
+    Message
+  },
+  methods: {
+    save(e) {
+      e.preventDefault();
+      if(this.myName !== null) {
+        this.nameSaved = true;
+      }
+    }
   }
+  
 }
 </script>
 
 <style>
+html {
+  background: #000;
+  color: #fff;
+  font-weight: 400;
+
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input {
+    background: #ffffff0d;
+    padding: 18px 20px;
+    outline: 0;
+    border: 0;
+    border-bottom: 4px solid #fff;
+    color: white;
+    font-family: inherit;
+    font-weight: 600;
+    font-size: 30px;
+}
+form {
+  display: flex;
+
+}
+button {
+      background: #220086;
+    border: 0;
+    font-size: 22px;
+    height: 1;
+    padding: 10px 40px;
+    color: white;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,47 @@
+
+
+
+<template>
+    <div class="message">
+      <span>Hello {{name}}</span>
+      <span>How can I help?</span>
+    </div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+
+export default {
+  name: 'Message',
+  props: ["userName"],
+  mounted() {
+    var tl = gsap.timeline();
+    tl.fromTo(".message > *:first-child", {y: 30, opacity: 0}, {y: 0, opacity: 1, duration: 1.4}, "+=0.8");
+    tl.to(".message > *:first-child", {y: -20, opacity: 0, duration: 1}, ">");
+    tl.fromTo(".message > *:last-child", {y: 30, opacity: 0}, {y: 0, opacity: 1, duration: 1.4}, ">");
+  },
+  computed: {
+      name() {
+          return this.userName;
+      }
+  }
+}
+</script>
+
+<style>
+
+.message {
+  font-size: 40px;
+  display: flex;
+  position: relative;
+  width: 100%;
+  text-align: center;
+  justify-content: center;
+}
+.message > * {
+  opacity: 0;
+  display: inline-flex;
+  position: absolute;
+  top: 0;
+}
+</style>
